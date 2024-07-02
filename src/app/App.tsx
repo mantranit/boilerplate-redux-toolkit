e4e470dcba3e4c2c846bca38e6212a887f260b86
Man Tran <tranduyminhman@gmail.com>
2024-07-02 09:44:24 +0700

    backto leaderboard

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,6 +11,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import Tracking from "./pages/Tracking";
 import Rule from "./pages/Rule";
+import Leaderboard from "./pages/Leaderboard";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -38,7 +39,7 @@ export default function App() {
             },
             {
               path: "/leaderboard",
-              element: <Tracking isLeaderboard={true} />,
+              element: <Leaderboard />,
             },
             {
               path: "/tracking",
